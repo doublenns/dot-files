97f0987bfc30998f183230f210acae8a27ccb568
DoubleNNs <nnamdianyanwu@gmail.com>
2018-01-21 23:56:28 +0000

    More python 2/3 generalizations

--- dot_file_install.py
+++ dot_file_install.py
@@ -3,7 +3,14 @@
 import os
 import sys
 import shutil
-import urllib
+if sys.version_info[0] == 2:
+    import urllib
+elif sys.version_info[0] == 3:
+    import urllib.request
+    import urllib.error #import URLError, HTTPError
+else:
+    print("Python version isn'2 2 or 3. Don't know how to proceed. Exiting")
+    sys.exit(1)
 
 
 def check_dotfiles(users_homedir, dotfiles):
@@ -13,18 +20,45 @@ def check_dotfiles(users_homedir, dotfiles):
     return False
 
 
-def copy_dotfiles(users_homedir, dotfiles):
-    pass
+def deploy_dotfiles(users_homedir, dotfiles):
+    '''
+    Function deploys the dotfiles into the current user's home dir..
+    1st checks to see if can copy the files from cloned repo, in the running
+    script's directory. If not, downloads the files from GitHub repo.
+    '''
+
+    script_dirname = os.path.dirname(os.path.abspath(__file__))
+    gitrepo_raw_url = "https://raw.githubusercontent.com/doublenns/dot-files/master/"
+
+    for dotfile in dotfiles:
+        # If dotfile is in calling script's path (cloned git repo)
+        dotfile_full_path = "{}/{}".format(script_dirname, dotfile)
+        if os.path.isfile( dotfile_full_path ):
+            shutil.copy(dotfile_full_path, "{}/.{}".format(users_homedir, dotfile))
+            print("Copied " + dotfile_full_path)
+        # If dotfile NOT in script's path (downloaded/executed single file)
+        else:
+            dotfile_url = gitrepo_raw_url + dotfile
+            print(dotfile_url)
+            try:
+                urllib.urlretrieve(dotfile_url, "{}/.{}".format(users_homedir, dotfile))
+            except AttributeError:
+                try:
+                    urllib.request.urlretrieve(dotfile_url, "{}/.{}".format(users_homedir, dotfile))
+                except urllib.error.HTTPError as e:
+                    print("Unable to download {} from GitHub. Error code: {}".format(
+                        dotfile, e.code))
+                except urllib.errorURLError as e:
+                    print("Unable to download {} from GitHub. Reasone: {}".format(
+                        dotfile, e.reason))
 
 
 def main():
-    script_dirname = os.path.dirname(os.path.abspath(__file__))
     users_homedir = os.path.expanduser("~")
     dotfiles = ("vimrc",
                     "bash_profile",
                     "gitconfig",
                     "poop")
-    gitrepo_raw_url = "https://raw.githubusercontent.com/doublenns/dot-files/master/"
 
     if check_dotfiles(users_homedir, dotfiles):
         global input
@@ -36,15 +70,7 @@ def main():
         if choice.lower() != "y":
             sys.exit(1)
 
-    for dotfile in dotfiles:
-        # If dotfile is in calling script's path (cloned git repo)
-        dotfile_full_path = "{}/{}".format(script_dirname, dotfile)
-        if os.path.isfile( dotfile_full_path ):
-            shutil.copy(dotfile_full_path, "{}/.{}".format(users_homedir, dotfile))
-        # If dotfile NOT in script's path (downloaded/executed single file)
-        else:
-            dotfile_url = gitrepo_raw_url + dotfile
-            urllib.urlretrieve(dotfile_url, "{}/.{}".format(users_homedir, dotfile))
+    deploy_dotfiles(users_homedir, dotfiles)
 
 
 if __name__ == "__main__":
