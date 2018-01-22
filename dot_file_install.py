8b06a82ff81eeb4ccaacb8de0545263cc93f5759
DoubleNNs <nnamdianyanwu@gmail.com>
2018-01-22 03:26:01 +0000

    Linting

--- dot_file_install.py
+++ dot_file_install.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import os
 import sys
 import shutil
@@ -7,15 +6,18 @@ if sys.version_info[0] == 2:
     import urllib2
 elif sys.version_info[0] == 3:
     import urllib.request
-    import urllib.error #import URLError, HTTPError
+    import urllib.error
 else:
     print("Python version isn'2 2 or 3. Don't know how to proceed. Exiting")
     sys.exit(1)
 
 
 def check_dotfiles(users_homedir, dotfiles):
+    '''
+    Function to check if dotfiles already exist
+    '''
     for dotfile in dotfiles:
-        if os.path.isfile( "{}/.{}".format(users_homedir, dotfile) ):
+        if os.path.isfile("{}/.{}".format(users_homedir, dotfile)):
             return True
     return False
 
@@ -28,7 +30,7 @@ def download_file(download_url, dest):
     # of error that occurs
     try:
         urllib.request.urlretrieve(download_url, dest)
-    except NameError: # Fails because Python2 instead of 3
+    except NameError:  # Fails because Python2 instead of 3
         try:
             response = urllib2.urlopen(download_url)
             with open(dest, "w") as dotfile:
@@ -48,21 +50,19 @@ def download_file(download_url, dest):
             download_url, e.reason))
 
 
-
 def deploy_dotfiles(users_homedir, dotfiles):
     '''
     Function deploys the dotfiles into the current user's home dir..
     1st checks to see if can copy the files from cloned repo, in the running
     script's directory. If not, downloads the files from GitHub repo.
     '''
-
     script_dirname = os.path.dirname(os.path.abspath(__file__))
     gitrepo_raw_url = "https://raw.githubusercontent.com/doublenns/dot-files/master/"
 
     for dotfile in dotfiles:
         # If dotfile is in calling script's path (cloned git repo)
         dotfile_full_path = "{}/{}".format(script_dirname, dotfile)
-        if os.path.isfile( dotfile_full_path ):
+        if os.path.isfile(dotfile_full_path):
             shutil.copy(dotfile_full_path, "{}/.{}".format(users_homedir, dotfile))
             print("Copied " + dotfile_full_path)
         # If dotfile NOT in script's path (downloaded/executed single file)
@@ -73,18 +73,21 @@ def deploy_dotfiles(users_homedir, dotfiles):
 
 
 def main():
+    '''
+    Script's main function
+    '''
     users_homedir = os.path.expanduser("~")
 
     # Manually insert which dotfiles want to ve managed here
     dotfiles = ("vimrc",
-                    "bash_profile",
-                    "gitconfig",
-                    "poop")
+                "bash_profile",
+                "gitconfig",
+                "poop")
 
     if check_dotfiles(users_homedir, dotfiles):
         global input
-        #try: input = raw_input
-        #except NameError: pass
+        # try: input = raw_input
+        # except NameError: pass
         if hasattr(__builtins__, 'raw_input'):
             input = raw_input
         choice = input("This script will overwrite existing dotfiles. Proceed? [y/N]")
