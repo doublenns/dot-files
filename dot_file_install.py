fc75c1d80beb5ada4e0f4c6310bdffe09bf414a8
DoubleNNs <nnamdianyanwu@gmail.com>
2019-10-19 21:28:15 +0000

    Implement file downloads using the requests module
    Originally, wanted to write the script using only the standard library
    to prevent having any external dependencies. Additionally wanted the
    script to pick up the local system's default version of Python,
    regardless of whether it was Python 2 or 3. This was so that the script
    could be effectively ran on almost any system without needing to modify
    the system -- no need to install anything new. However, requests is the
    de-facto standard for making HTTP requests in Python and Python 2 is
    going to be EoL soon. (Addiitonally, my methods of dynamically having
    the script work for both Python 2 and 3 were very, very poor :) I should
    have used the six PyPI compabaility ibrary instead :)

--- dot_file_install.py
+++ dot_file_install.py
@@ -3,8 +3,8 @@ import os
 import sys
 import shutil
 import distutils
-import urllib.request
-import urllib.error
+import requests
+import subprocess
 from distutils import dir_util
 
 
@@ -39,27 +39,10 @@ def download_file(download_url, dest):
     '''
     Function to download file from URL
     '''
-    # Needs better error handling here. Only works now based on order
-    # of error that occurs
-    try:
-        urllib.request.urlretrieve(download_url, dest)
-    except NameError:  # Fails because Python2 instead of 3
-        try:
-            response = urllib2.urlopen(download_url)
-            with open(dest, "w") as dotfile:
-                dotfile.write(response.read())
-        except urllib2.HTTPError as e:
-            print("Unable to download {download_url} from GitHub. Error code: {e.code}".)
-        except urllib2.URLError as e:
-            print("Unable to download {} from GitHub. Reasone: {}".format(
-                download_url, e.reason))
-    # Python3 but fails because of HTTP/URL issues
-    except urllib.error.HTTPError as e:
-        print("Unable to download {} from GitHub. Error code: {}".format(
-            download_url, e.code))
-    except urllib.error.URLError as e:
-        print("Unable to download {} from GitHub. Reasone: {}".format(
-            download_url, e.reason))
+    response = requests.get(download_url)
+    if response:
+        with open(dest, "wb") as f:
+            f.write(response.content)
 
 
 def download_dir(download_url, dest):
@@ -77,18 +60,17 @@ def deploy_dotfiles(users_homedir, dotfiles, dotdirs):
     '''
     script_dirname = os.path.dirname(os.path.abspath(__file__))
     gitrepo_raw_url = "https://raw.githubusercontent.com/doublenns/dot-files/master/"
-    gitrepo_svn_url= "https://raw.githubusercontent.com/doublenns/dot-files/trunk/"
+    gitrepo_svn_url= "https://github.com/doublenns/dot-files/trunk/"
 
     for dotfile in dotfiles:
         # If dotfile is in calling script's path (cloned git repo)
         dotfile_full_path = "{}/{}".format(script_dirname, dotfile)
         if os.path.isfile(dotfile_full_path):
             shutil.copy(dotfile_full_path, "{}/.{}".format(users_homedir, dotfile))
-            # print("Copied " + dotfile_full_path)
         # If dotfile NOT in script's path (downloaded/executed single file)
         else:
             dotfile_url = gitrepo_raw_url + dotfile
-            dest = "{}/.{}".format(users_homedir, dotfile)
+            dest = f"{users_homedir}/.{dotfile}"
             download_file(dotfile_url, dest)
 
     for dotdir in dotdirs:
@@ -99,7 +81,7 @@ def deploy_dotfiles(users_homedir, dotfiles, dotdirs):
             # print("Copied " + dotdir_full_path)
         # If dotdir NOT in script's path (downloaded/executed single file)
         else:
-            dotdir_url = gitrepo_svn_url + dotfile
+            dotdir_url = gitrepo_svn_url + dotdir
             dest = "{}/.{}".format(users_homedir, dotdir)
             download_dir(dotdir_url, dest)
 
@@ -121,8 +103,6 @@ def main():
 
     dotlocations = dotfiles + dotdirs
     dot_conflict = check_dotlocations(users_homedir, dotlocations)
-    print("The dot conflicts are!!")
-    print(dot_conflict)
     if dot_conflict:
         global input
         # try: input = raw_input
@@ -131,7 +111,9 @@ def main():
             input = raw_input
         choice = input("This script will overwrite existing dotfiles. Proceed? [y/N]")
         if choice.lower() != "y":
-            print(f"You already have {dot_conflict} located in your home directory")
+            print("You already have the following files located in your home directory:")
+            print(", ".join(dot_conflict))
+            print()
             sys.exit(1)
 
     deploy_dotfiles(users_homedir, dotfiles, dotdirs)
